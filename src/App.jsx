39184d1a6e87b5f0baa9024d16ff761fff45999a
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-07-17 20:50:23 -0300

    Navbar and Contents
    - Navbar created
    - Contents created
    - Styles added

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,25 @@ import "./App.css"
 
 function App() {
 	return (
-		<>
-			<h1>Diego!</h1>
-		</>
+		<div className="App">
+			<nav>Navbar</nav>
+
+			<div className="content-div" style={{ height: "80vh" }}>
+				Content 1
+			</div>
+			<div className="content-div" style={{ height: "50vh" }}>
+				Content 2
+			</div>
+			<div className="content-div" style={{ height: "60vh" }}>
+				Content 3
+			</div>
+			<div className="content-div" style={{ height: "40vh" }}>
+				Content 4
+			</div>
+			<div className="content-div" style={{ height: "100vh" }}>
+				Content 5
+			</div>
+		</div>
 	)
 }
 
